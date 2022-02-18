a9921869a6c4a6b0c18baef2ff172ebc376ccacd
hkreshock <hkreshock@gmail.com>
2022-02-18 13:51:55 -0700

    Empty template

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,5 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <Header></Header>
-    <Drawer :essentialLinks="essentialLinks"></Drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -10,8 +7,6 @@
 </template>
 
 <script>
-import Drawer from 'components/navigation/SiteNavDrawer.vue';
-import Header from 'components/navigation/SiteHeader.vue';
 
 import { defineComponent, ref } from 'vue';
 
@@ -19,36 +14,10 @@ export default defineComponent({
     name: 'MainLayout',
 
     components: {
-        Drawer,
-        Header,
     },
 
     setup() {
-        const linkList = [
-            {
-                title: 'Dashboard',
-                caption: '',
-                icon: 'search',
-                link: '/',
-            },
-            {
-                title: 'Login',
-                caption: '',
-                icon: 'mdi-account-outline',
-                link: '/login',
-            },
-            // {
-            //     title: 'FooComponent',
-            //     caption: '',
-            //     icon: 'mdi-finance',
-            //     link: '/fooComponent',
-            // },
-        ];
-
-        // can rename vars when returning to template
-        return {
-            essentialLinks: linkList,
-        };
+        return { };
     },
 });
 </script>
