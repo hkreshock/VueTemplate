8472dbe19f148fc1d6bee25b34c8a7c29c92383f
hkreshock <hkreshock@gmail.com>
2022-02-21 15:12:21 -0700

    Dashboard update

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,6 +1,5 @@
 <template>
     <q-page class="flex flex-center">
-        <StateTable></StateTable>
     </q-page>
 </template>
 
@@ -11,12 +10,9 @@ import { useStore } from 'vuex';
 // animations
 import 'animate.css';
 
-import StateTable from '../components/pages/dashboard';
-
 export default defineComponent({
     name: 'Dashboard',
     components: {
-        StateTable,
     },
     setup() {
         const $store = useStore();
